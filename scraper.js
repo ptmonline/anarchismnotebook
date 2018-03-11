0f000de336ffb12155c81dd08d292c41d447232b
ptm <ptorresonline@gmail.com>
2018-03-11 17:29:33 +0100

    added scraper

---
+++ scraper.js
@@ -0,0 +1,27 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var colors = require('colors');
+ 
+
+function searchItems(){
+  console.log("searching... https://www.goodreads.com/quotes/tag/anarchism")
+  request({
+    uri: "https://www.goodreads.com/quotes/tag/anarchism",
+  }, function(error, response, body) {
+    var $ = cheerio.load(body);
+    var quote = $('.quote.mediumText');
+    
+      $(quote).each(function(){
+        var punctuation = $(this).find('.quoteText');
+        var author = $(punctuation).find('.authorOrTitle');
+
+          console.log(colors.green($(author).text()));
+        
+
+      })
+    
+  });
+
+}
+
+searchItems();
