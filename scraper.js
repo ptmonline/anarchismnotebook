c33805ca5a66f1874c09e77e0090ee44913cd930
ptmonline <ptmonlinework@gmail.com>
2018-03-12 10:18:57 +0100

    removed whitespace

--- scraper.js
+++ scraper.js
@@ -1,27 +1,21 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var colors = require('colors');
- 
 
-function searchItems(){
+
+function searchItems() {
   console.log("searching... https://www.goodreads.com/quotes/tag/anarchism")
   request({
     uri: "https://www.goodreads.com/quotes/tag/anarchism",
-  }, function(error, response, body) {
+  }, function (error, response, body) {
     var $ = cheerio.load(body);
     var quote = $('.quote.mediumText');
-    
-      $(quote).each(function(){
-        var punctuation = $(this).find('.quoteText');
-        var author = $(punctuation).find('.authorOrTitle');
 
-          console.log(colors.green($(author).text()));
-        
-
-      })
-    
+    $(quote).each(function () {
+      var punctuation = $(this).find('.quoteText');
+      var author = $(punctuation).find('.authorOrTitle');
+      console.log(colors.green($(author).text()));
+    })
   });
-
 }
-
 searchItems();
