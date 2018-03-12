81f9d4c9347dd895e45b042a1608860e6413d499
ptmonline <ptmonlinework@gmail.com>
2018-03-12 17:47:43 +0100

    added images + styles

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,26 +14,43 @@ export class HomePage {
   public text: string;
   public id: number;
   public subtext: string;
+  public extract: string;
+  public src: any;
 
   constructor(public navCtrl: NavController, private _api: Api) {
     this.init();
   }
 
-  init(){
-    this._api.get('quotes.json').subscribe((data)=>{
+  init() {
+    this._api.get('quotes.json').subscribe((data) => {
       this.allQuotes = data;
       this.chooseQuote();
       console.log(this.allQuotes.quotes);
     })
   }
 
-  chooseQuote(){
+  chooseQuote() {
     this.showQuote = this.allQuotes.quotes[Math.floor(Math.random() * this.allQuotes.quotes.length)];
     console.log(this.showQuote);
-    this.id = this.showQuote.id;
-    this.author = this.showQuote.author;
-    this.text = this.showQuote.text;
-    if(this.showQuote.subtext) this.subtext = this.showQuote.subtext
+    if (this.showQuote.img == null) {
+      this.id = this.showQuote.id;
+      this.author = this.showQuote.author;
+      this.text = this.showQuote.text;
+      this.src = null;
+      (this.showQuote.extract) ? this.extract = this.showQuote.extract : this.extract = null;
+      (this.showQuote.subtext) ? this.subtext = this.showQuote.subtext : this.subtext = null;
+    } else {
+      this.src = './assets/imgs/' + this.showQuote.img;
+      this.getHeight();
+    }
+  }
+
+  getHeight() {
+    setTimeout(() => {
+      let imageHeight = document.getElementById('imgId');
+      let newHeight = ((window.innerHeight - imageHeight.clientHeight) / 2) - 16 ;
+      imageHeight.style.marginTop = newHeight.toString() + 'px';
+    }, 50)
   }
 
 }
