5f5760c5b0db2ceeef4caca89de66e0eb67a9933
ptmonline <ptmonlinework@gmail.com>
2018-03-16 17:48:30 +0100

    work on swipe directions

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,6 +14,7 @@ export class HomePage {
   public src: boolean;
   public rndClass: string;
   public scrClass: string;
+  public quotesArray: any = [];
 
   constructor(public navCtrl: NavController, private _api: Api) {
     this.init();
@@ -30,11 +31,14 @@ export class HomePage {
   swipeEvent(ev?: any){
     if(ev) console.log('DIRECTION: ',ev.direction);
     if(ev.direction == 2) this.chooseQuote();
+    if(ev.direction == 4) this.choosePreviousQuote();
   }
 
   chooseQuote(): void {
 
     this.showQuote = this.allQuotes.quotes[Math.floor(Math.random() * this.allQuotes.quotes.length)];
+    this.quotesArray.push(this.showQuote.id);
+    console.log(this.quotesArray)
       this.src = false;
       this.scrClass = null;
       let quoter = this.showQuote.text;
@@ -46,6 +50,17 @@ export class HomePage {
       }
       console.log(this.rndClass);
     
+  }
+
+  choosePreviousQuote(){
+    if(this.quotesArray == null || this.quotesArray == 'empty'){
+      return null;
+    }else{
+      console.log('QUOTE PREV: ', this.quotesArray[this.quotesArray.length -1]);
+      this.showQuote = this.allQuotes.quotes[this.quotesArray.length -2];
+      console.log('PREV: ', this.showQuote);
+      this.quotesArray.pop();
+    }
   }
 
   getRandomClass() {
