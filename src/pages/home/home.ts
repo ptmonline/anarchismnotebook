37ca67823ccc24f874cc0a2fed2c9da93afd9821
Pau Torres <ptmonlinework@gmail.com>
2018-03-18 18:35:48 +0100

    added new style

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,12 +11,12 @@ export class HomePage {
 
   public allQuotes: any;
   public showQuote: Api.Client.Quote;
-  public src: boolean;
   public rndClass: string;
   public scrClass: string;
   public quotesArrayInit: any = [];
   public quotesArray: any = [];
   public n: number = 0;
+  public state4: boolean = false;
 
   constructor(public navCtrl: NavController, private _api: Api) {
     this.init();
@@ -30,43 +30,47 @@ export class HomePage {
     })
   }
 
-  chooseQuoteInit(){
+  chooseQuoteInit() {
     this.showQuote = this.quotesArrayInit[this.n];
     this.quotesArray.push(this.showQuote.id);
     this.buildQuote();
   }
 
-  nextQuote(){
+  nextQuote() {
     this.n += 1;
     this.showQuote = this.quotesArrayInit[this.n];
-    if (this.quotesArray.indexOf(this.showQuote.id) < 0) this.quotesArray.push(this.showQuote.id)
+    if (this.quotesArray.indexOf(this.showQuote.id) < 0) this.quotesArray.push(this.showQuote.id);
+    console.log(this.quotesArray);
     this.buildQuote();
   }
-  prevQuote(){
+  prevQuote() {
     this.n -= 1;
-    if(this.n >= 0) {
-      this.showQuote = this.quotesArrayInit[this.n];
-    } else {
-      this.n = 0;
-      this.showQuote = this.quotesArrayInit[this.n];
-    }
+    (this.n >= 0) ? this.showQuote = this.quotesArrayInit[this.n] : this.showQuote = this.quotesArrayInit[0]
     this.buildQuote();
   }
 
-  swipeEvent(ev?: any){
-    if(ev) console.log('DIRECTION: ',ev.direction);
-    if(ev.direction == 2) this.nextQuote();
-    if(ev.direction == 4) this.prevQuote();
+  swipeEvent(ev?: any) {
+    if (ev) console.log('DIRECTION: ', ev.direction);
+    if (ev.direction == 2) this.nextQuote();
+    if (ev.direction == 4) this.prevQuote();
   }
 
-  buildQuote(){
-    this.src = false;
+  buildQuote() {
+
     this.scrClass = null;
     let quoter = this.showQuote.text;
     if (quoter.length <= 250 && this.showQuote.extract != null) {
       this.rndClass = this.getRandomClass2();
-    }else{
+      console.log(this.rndClass);
+      if (this.rndClass == 'randclass randclass-5') {
+        this.state4 = true;
+        console.log("STATE 4 ACTIVATED");
+      } else {
+        this.state4 = false;
+      }
+    } else {
       this.rndClass = this.getRandomClass();
+      this.state4 = false;
     }
   }
 
@@ -75,28 +79,28 @@ export class HomePage {
   }
 
   getRandomClass2() {
-    return "randclass randclass-" + (Math.floor(Math.random() * 2) + 3);
+    return "randclass randclass-" + (Math.floor(Math.random() * 3) + 3);
   }
 
   getHeight() {
-    
-      let quoteHeight = document.getElementById('center-quote');
-      console.log(quoteHeight.clientHeight);
- 
+
+    let quoteHeight = document.getElementById('center-quote');
+    console.log(quoteHeight.clientHeight);
+
   }
 
   shuffle(array: string[]) {
-        let currentIndex = array.length,
-            temporaryValue,
-            randomIndex;
-        while (0 !== currentIndex) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
-        return array;
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
     }
+    return array;
+  }
 
 }
